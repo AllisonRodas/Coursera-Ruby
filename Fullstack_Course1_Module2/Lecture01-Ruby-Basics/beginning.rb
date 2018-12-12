5391fc6fa446c03cdc3acd9d836237db6ea7fd78
Allison T. Rodas <allisonrodas58@gmail.com>
2018-12-12 14:27:20 -0500

    Adding Module two to Github

---
+++ Fullstack_Course1_Module2/Lecture01-Ruby-Basics/beginning.rb
@@ -0,0 +1,14 @@
+3.times { puts "Hello World" }
+
+# this is a comment 
+puts 5 # so is this
+3 # and this
+
+p "Got it" # => Got it
+
+#new lines by Allison
+5.times { puts "Let's rule the world" }
+
+puts 5*8
+
+p "I think I got it"
