b5c5bf95a121a3e04100ba03648158c43b7f204f
Allison T. Rodas <allisonrodas58@gmail.com>
2018-12-12 17:15:24 -0500

    First change on code

--- Fullstack_Course1_Module2/Lecture04-Blocks/explicit_blocks.rb
+++ Fullstack_Course1_Module2/Lecture04-Blocks/explicit_blocks.rb
@@ -7,3 +7,16 @@ end
 puts two_times_explicit # => No block 
 two_times_explicit { puts "Hello"} # => Hello 
                                    # => Hello 
+
+
+#New code by Allison
+p "---New code by Allison---"
+
+def times_explicit (&im_a_block) 
+  	return "No block" if im_a_block.nil? 
+  	im_a_block.call
+end 
+
+puts times_explicit
+times_explicit { puts 45; puts "a"}
+times_explicit
